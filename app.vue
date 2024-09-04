6b5b976d7cae1a1973dfa283b84a59e935a3ea5d
Kevin Kinner <kevin.kinner@css.de>
2024-09-04 13:43:01 +0200

    Top part frontend like semi good :)

--- app.vue
+++ app.vue
@@ -1,6 +1,48 @@
-<template>
+import {ref, onMounted} from 'vue';
+import number from './comnponents/Number.vue';
+
+<template lang="html">
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="headLine">WE´RE LAUNCHING SOON</h1>
+    <div class="numberCounterWrapper">
+    <number number="08" label="Days"></number>
+    <number number="23" label="Hours"></number>
+    <number number="55" label="Minutes"></number>
+    <number number="41" label="Seconds"></number>
+    </div>
   </div>
 </template>
+<script lang="js">
+</script>
+<style lang="css">
+@import url("https://fonts.googleapis.com/css2?family=Red+Hat+Text:ital,wght@0,300..700;1,300..700&display=swap");
+#__nuxt {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+body {
+  background-color: #1e1e2a;
+  background-image: url("images\bg-stars.svg");
+  color: #ffffff;
+  font-family: "Red Hat Text", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 700;
+  font-style: normal;
+  font-size: 14px;
+}
+
+.headLine {
+  margin-top: 20vh;
+  letter-spacing: 4px;
+  font-size: 21px;
+}
+.numberCounterWrapper {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5vh;
+}
+</style>
---
+++ components/Number.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+    <div class="numberWrapperWrapper">
+        <div class="numberWrapper">
+            {{number}}
+        </div>
+        <label>{{label}}</label>
+    </div>
+</template>
+<script setup lang="js">
+    defineProps(
+        {
+            number: Number,
+            label: String
+        }
+    );
+
+</script>
+<style lang="css">
+.numberWrapper {
+  display: flex;
+  width: 10vh;
+  height: 10vh;
+  background-color: grey;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15px;
+  font-size: 2rem;
+  color: hsl(345, 95%, 68%);
+  margin-bottom: 10px;
+}
+
+.numberWrapperWrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  font-size: 0.6rem;
+  width: 100%;
+}
+
+.numberWrapperWrapper > label {
+  font-size: 0.6rem;
+  opacity: 0.7;
+  font-weight: 200;
+  letter-spacing: 2px;
+}
+</style>
